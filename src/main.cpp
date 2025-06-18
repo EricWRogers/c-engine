a99d854e9c45416492eb3059381d18c531b01490
EricWRogers <ericwilliamrogers@gmail.com>
2025-06-17 23:06:48 -0500

    start test ground for lua and game engine

--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,16 @@
 #include <vector>
 #include <stdio.h>
-#include <iostream>
 
-#include "core/String.hpp"
+namespace String {
+    
+}
 
-struct Task {
-    bool complete;
-    String name;
-};
+namespace Window {
+
+}
 
 int main()
 {
-    int* nums = nullptr;
-    List::Init(&nums, 1, sizeof(int));
-    for(int i = 0; i < 100; i++)
-        List::Add(&nums, &i);
-
-    String message("Hello, World!");
-    String p = ".";
-
-    printf("Before assignment\n");
-    p = message;
-    printf("After assignment\n");
-
-    printf("Message: %s\n", message.CString());
-    printf("P: %s\n", p.CString());
-
-    message = message + " From Eric" + p;
-
-    printf("h\n");
-
-    printf("Message: %s\n", message.CString());
-
-    printf("Start\n");
-
-    Task* tasks = nullptr;
-    List::Init(&tasks, 1, sizeof(Task));
-
-    for (int i = 0; i < 10; i++)
-    {
-        
-        Task t = {};
-        t.complete = false;
-        t.name = "task name";
-
-        List::Add(&tasks, &t);
-    }
-
-    for (int i = 0; i < List::GetCount(&tasks); i++)
-    {
-        Task* task = &tasks[i];
-        printf("%s\n", task->name.CString());
-    }
-
-    printf("End\n");
-
+    printf("Hello, World");
     return 0;
 }

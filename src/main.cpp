9c754a0e57b73f3b57d2bf554cdc9594d8e523a2
EricWRogers <ericwilliamrogers@gmail.com>
2025-06-17 23:53:48 -0500

    set up sol and lua

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,17 @@ namespace Window {
 
 }
 
-int main()
-{
-    printf("Hello, World");
-    return 0;
+#include <iostream>
+
+#include <sol/sol.hpp>
+
+int main() {
+    std::cout << "Hello, from C++\n";
+    sol::state lua;
+    lua.open_libraries(sol::lib::base, sol::lib::package);
+    sol::protected_function_result result = lua.script("print('Hello from Lua')");
+    if (!result.valid()) {
+        sol::error err = result;
+        std::cout << err.what();
+    }
 }

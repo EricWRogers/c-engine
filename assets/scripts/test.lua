28065611dafaa80227a6175dd034e0a1e81f193f
EricWRogers <ericwilliamrogers@gmail.com>
2025-06-18 23:32:58 -0500

    switch to script file

---
+++ assets/scripts/test.lua
@@ -0,0 +1,1 @@
+print("Hello Script File!\n")
--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,8 @@ int main() {
     std::cout << "Hello, from C++\n";
     sol::state lua;
     lua.open_libraries(sol::lib::base, sol::lib::package);
-    sol::protected_function_result result = lua.script("print('Hello from Lua')");
+    
+    sol::protected_function_result result = lua.do_file("assets/scripts/test.lua");
     if (!result.valid()) {
         sol::error err = result;
         std::cout << err.what();

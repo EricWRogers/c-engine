0ae2f63336d88777af523b4eadfad85216230c10
EricWRogers <ericwilliamrogers@gmail.com>
2025-06-19 23:50:53 -0500

    drop sol for now

--- assets/scripts/test.lua
+++ assets/scripts/test.lua
@@ -1,2 +1,9 @@
-print("Hello Script File!\n")
-hello_cpp()
+
+function Start()
+    x = "5"
+    Log("Start " .. x)
+end
+
+function Update()
+    Log("Update")
+end
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,81 @@
 #include <vector>
 #include <stdio.h>
-
-namespace String {
-    
-}
-
-namespace Window {
-
-}
-
 #include <iostream>
+#include <string>
 
-#include <sol/sol.hpp>
+extern "C" {
+    #include <lua.h>
+    #include <lualib.h>
+    #include <lauxlib.h>
+}
 
-void HelloCPP()
+const int MAX_STRING = 512;
+char stringBuilderBuffer[MAX_STRING];
+
+void error(const char* _message)
 {
-    printf("HelloCPP\n");
+    printf("error: %s\n", _message);
+}
+
+void printError(lua_State* state)
+{
+    error(lua_tostring(state, (int)-1));
+    lua_pop(state, 1); // remove error message from top of stack
+}
+
+void RunScript(lua_State* state, const char* file)
+{
+    int result = luaL_loadfile(state, file);
+
+    if (result == LUA_OK)
+    {
+        // use pcall to execute the script.
+        result = lua_pcall(state, 0, LUA_MULTRET, 0);
+
+        if (result != LUA_OK)
+        {
+            printError(state);
+        }
+    }
+    else
+    {
+        printError(state);
+    }
+}
+
+void Call(lua_State* state, const char* functionName)
+{
+    // pushes onto the stack the value of the global name of the lua function to be called.
+    int type = lua_getglobal(state, functionName);
+
+    if (type == LUA_TNIL)
+    {
+        snprintf(stringBuilderBuffer, sizeof(stringBuilderBuffer), "Attempted to call undefined Lua function: %s", functionName);
+        error(stringBuilderBuffer);
+    }
+    else if (lua_pcall(state, 0, 0, 0) != 0)
+    {
+        printError(state);
+    }
+}
+
+int Log(lua_State *_state)
+{
+    const char* text = lua_tostring(_state, (int)1);
+    printf("%s\n", text);
+    return 0;
 }
 
 int main() {
-    std::cout << "Hello, from C++\n";
-    sol::state lua;
-    lua.open_libraries(sol::lib::base, sol::lib::package);
-    lua["hello_cpp"] = HelloCPP;
-    
-    sol::protected_function_result result = lua.do_file("assets/scripts/test.lua");
-    if (!result.valid()) {
-        sol::error err = result;
-        std::cout << err.what();
-    }
+    lua_State* luaState = luaL_newstate();
+    luaL_openlibs(luaState);
+
+    lua_register(luaState, "Log", Log);
+
+    RunScript(luaState, "assets/scripts/test.lua");
+
+    Call(luaState, "Start");
+    Call(luaState, "Update");
+
+    lua_close(luaState);
 }

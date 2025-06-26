a84b3be53dc7f5390bd1aed9a207b1e4626515a4
EricWRogers <ericwilliamrogers@gmail.com>
2025-06-26 08:41:57 -0500

    try anglescript

--- src/main.cpp
+++ src/main.cpp
@@ -11,16 +11,7 @@ namespace Window {
 
 #include <iostream>
 
-#include <sol/sol.hpp>
-
 int main() {
     std::cout << "Hello, from C++\n";
-    sol::state lua;
-    lua.open_libraries(sol::lib::base, sol::lib::package);
-    
-    sol::protected_function_result result = lua.do_file("assets/scripts/test.lua");
-    if (!result.valid()) {
-        sol::error err = result;
-        std::cout << err.what();
-    }
+    return 0;
 }

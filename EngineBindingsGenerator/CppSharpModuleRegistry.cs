92e411a0550c6d8fff5b6af1e702a76f74cac27d
Azee <aziztitu1996@gmail.com>
2025-07-13 22:10:31 -0700

    Cleans up CppSharpModuleRegistry

--- EngineBindingsGenerator/CppSharpModuleRegistry.cs
+++ EngineBindingsGenerator/CppSharpModuleRegistry.cs
@@ -7,8 +7,6 @@
                 Name = "CanisEngine",
                 IncludeDirs = ["../src", "../src/scripting", "../external/glm", "../external/SDL/include"],
                 Headers = [
-                    //".generated/ScriptBindings.cpp",
-                    
                     // Canis
                     "canis/Window.hpp",
 
@@ -17,18 +15,6 @@
                     "CanisWindow.hpp",
                 ],
             },
-            //new CppSharpModuleInfo() {
-            //    Name = "CanisEngine2",
-            //    IncludeDirs = ["../src", "../src/scripting", "../external/glm", "../external/SDL/include"],
-            //    Headers = [
-            //        //".generated/ScriptBindings.cpp",
-            //        // Core
-            //        //"core/vec.h",
-
-            //        // Canis
-            //        "canis/Window.hpp",
-            //    ],
-            //},
         ];
     }
 }

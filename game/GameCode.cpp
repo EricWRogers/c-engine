af958530713970699dd1f05db5e6e593585bc288
EricWRogers <ericwilliamrogers@gmail.com>
2025-07-26 21:43:03 -0500

    using sdl to load shared lib

--- game/GameCode.cpp
+++ game/GameCode.cpp
@@ -1,16 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h> 
 #include <SDL3/SDL_log.h>
 extern "C" {
 
-void GameInit() {
+struct GameData {
+    int id = 5;
+};
+
+void* GameInit() {
     SDL_Log("Game initialized!");
+    GameData* gameData = (GameData*)malloc(sizeof(GameData));
+    gameData->id = 5;
+    return (void*) gameData;
 }
 
-void GameUpdate(float dt) {
-    SDL_Log("Game update %.2f", dt);
+void GameUpdate(float dt, void* _data) {
+    GameData& gameData = *(GameData*)_data;
+    SDL_Log("Game update %.2f %d", dt, gameData.id);
 }
 
-void GameShutdown() {
+void GameShutdown(void* _data) {
     SDL_Log("Game shutdown");
+    SDL_free(_data);
 }
 
 }
--- game/GameCode.hpp
+++ game/GameCode.hpp
@@ -3,12 +3,12 @@
 extern "C" {
 
 // Called once at startup
-void GameInit();
+void* GameInit();
 
 // Called every frame with delta time
-void GameUpdate(float dt);
+void GameUpdate(float dt, void* _data);
 
 // Called once at shutdown
-void GameShutdown();
+void GameShutdown(void* _data);
 
 }

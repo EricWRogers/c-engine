4ce709bca21d542724dcfeb1dcf817d2e57c7b17
EricWRogers <ericwilliamrogers@gmail.com>
2025-07-27 22:16:48 -0500

    default init

--- game/GameCode.cpp
+++ game/GameCode.cpp
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h> 
 #include <SDL3/SDL_log.h>
-#include <Canis/GameCodeObject.hpp>
 
-extern "C" {
+#include <Canis/App.hpp>
+#include <Canis/GameCodeObject.hpp>
 
 struct GameData {
     int id = 5;
+    int counter = 0;
 };
 
+extern "C" {
 void* GameInit() {
     SDL_Log("Game initialized!");
     GameData* gameData = (GameData*)malloc(sizeof(GameData));
-    gameData->id = 20;
+    *gameData = GameData{};
     return (void*) gameData;
 }
 
 void GameUpdate(float dt, void* _data) {
     GameData& gameData = *(GameData*)_data;
-    SDL_Log("Game update %.2f %d", dt, gameData.id);
+    SDL_Log("Game update %.2f %d Counter %d", dt, gameData.id, gameData.counter++);
 }
 
 void GameShutdown(void* _data) {

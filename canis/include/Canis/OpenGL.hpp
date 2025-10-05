6d01132fa9f6119365b84cca7f22d702bdc12982
EricWRogers <ericwilliamrogers@gmail.com>
2025-10-04 21:43:42 -0500

    Remove Comments

--- canis/include/Canis/OpenGL.hpp
+++ canis/include/Canis/OpenGL.hpp
@@ -1,8 +1,6 @@
 #pragma once
 
-//#include <GLES3/gl3.h>
+#include <GLES3/gl3.h>
 #include <SDL3/SDL_opengl.h>
 #include <SDL3/SDL_opengl_glext.h>
-#include <GLES3/gl3.h>
-//#include <SDL3/SDL_opengles2_gl2.h>
 const static char* OPENGLVERSION = "#version 310 es"; 

d6ab673bc14f3567f532af359acc0f1689a4f311
EricWRogers <ericwilliamrogers@gmail.com>
2025-12-15 20:32:30 -0600

    Simple Tank Bullets

---
+++ game/include/TankGame/Bullet.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <Canis/Entity.hpp>
+
+namespace TankGame
+{
+    class Bullet : public Canis::ScriptableEntity
+    {
+    private:
+        Canis::RectTransform* m_transform = nullptr;
+    public:
+        float speed = 10.0f;
+        float lifeTime = 25.0f;
+
+        Bullet(Canis::Entity &_entity) : Canis::ScriptableEntity(_entity) {}
+
+        void Create();
+        void Ready();
+        void Destroy();
+        void Update(float _dt);
+        void EditorInspectorDraw();
+    };
+
+    extern void RegisterBulletScript(Canis::App& _app);
+    extern void UnRegisterBulletScript(Canis::App& _app);
+}
---
+++ game/src/TankGame/Bullet.cpp
@@ -0,0 +1,69 @@
+#include <TankGame/Bullet.hpp>
+
+#include <Canis/App.hpp>
+#include <Canis/Time.hpp>
+#include <Canis/Math.hpp>
+#include <Canis/Scene.hpp>
+#include <Canis/Window.hpp>
+#include <Canis/Editor.hpp>
+#include <Canis/InputManager.hpp>
+
+#include <ConfigHelper.hpp>
+
+using namespace Canis;
+
+namespace TankGame
+{
+
+    ScriptConf conf = {};
+
+    void RegisterBulletScript(Canis::App &_app)
+    {
+        REGISTER_PROPERTY(conf, TankGame::Bullet, speed, float);
+        REGISTER_PROPERTY(conf, TankGame::Bullet, lifeTime, float);
+
+        DEFAULT_CONFIG_AND_REQUIRED(conf, TankGame::Bullet, Canis::RectTransform, Canis::Sprite2D);
+
+        conf.DrawInspector = [](Editor &_editor, Entity &_entity, const ScriptConf &_conf) -> void
+        {
+            TankGame::Bullet *bullet = nullptr;
+            if ((bullet = _entity.GetScript<TankGame::Bullet>()) != nullptr)
+            {
+                ImGui::InputFloat(("speed##" + _conf.name).c_str(), &bullet->speed);
+                ImGui::InputFloat(("lifeTime##" + _conf.name).c_str(), &bullet->lifeTime);
+            }
+        };
+
+        _app.RegisterScript(conf);
+    }
+
+    DEFAULT_UNREGISTER_SCRIPT(conf, Bullet)
+
+    void Bullet::Create() { Debug::Log("Tank But No Bullet!"); }
+
+    void Bullet::Ready() {
+        m_transform = entity.GetScript<Canis::RectTransform>();
+    }
+
+    void Bullet::Destroy() { Debug::Log("Kill Bullet But No Bullet!"); }
+
+    void Bullet::Update(float _dt) {
+        m_transform->Move(m_transform->GetRight() * speed * _dt);
+
+        lifeTime -= _dt;
+
+        Debug::Log("LifeTime: %f", lifeTime);
+
+        if (lifeTime <= 0.0f) {
+            Scene* scene = entity.scene;
+            int id = entity.id;
+
+            Debug::Log("id %d", id);
+
+            entity.Destroy();
+        }
+        
+    }
+
+    void Bullet::EditorInspectorDraw() {}
+}
